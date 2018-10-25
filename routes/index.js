021378f6eed1d8657f81a372c2cbc83009753172
Jonathan James <jjamesnwa1011@gmail.com>
2018-10-25 12:21:47 -0700

    added a section in routes for post request handlers

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello', function(req, res, next) {
+  res.send('HELLLOOOOOOO!');
+});
+
+
+/* POST home page */
+
 module.exports = router;
