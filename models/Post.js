c00e95562991dd158d1b1ebfe68bb6620777ecdd
Jonathan James <jjamesnwa1011@gmail.com>
2018-10-30 21:03:10 -0700

    added a mongo index for auto document expiration

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,21 @@
 var mongoose = require('mongoose');
 
-
 var postSchema = new mongoose.Schema({
   title: String,
   email: String,
-  description: String
+  description: String,
+  address: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  expireAt: {
+    type: Date,
+    default: null,
+  }
 });
 
+postSchema.index({expireAt: 1}, {expireAfterSeconds: 0});
 
 //how to add a method if needed:
 // postSchema.methods.example = function(){
