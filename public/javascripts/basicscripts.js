555556688100747ad53db14178508965ea7a954a
Jonathan James <jjamesnwa1011@gmail.com>
2018-11-01 20:23:57 -0700

    fixed the issue where the show-address button needed to be clicked twice before it would show the address

--- public/javascripts/basicscripts.js
+++ public/javascripts/basicscripts.js
@@ -1,10 +1,9 @@
 function hideshow() {
   console.log('fired');
   var x = document.getElementById('hideshow');
-  if(x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
+  if(x.style.display == 'block') {
     x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
   }
-
 }
