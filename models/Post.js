38a8ce512f4aaa87371d8433bea021146933faff
Jonathan James <jjamesnwa1011@gmail.com>
2018-11-13 13:47:31 -0800

    updated the address form and post model also merged first and last name into just fullname

--- models/Post.js
+++ models/Post.js
@@ -4,10 +4,14 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var postSchema = new mongoose.Schema({
   title: String,
   email: String,
-  firstName: String,
-  lastName: String,
+  fullName: String,
   description: String,
-  address: String,
+  addressLine1: String,
+  addressLine2: String,
+  city: String,
+  stateProvinceRegion: String,
+  zipOrPostal: Number,
+  country: String,
   delKey: ObjectId,
   createdAt: {
     type: Date,
