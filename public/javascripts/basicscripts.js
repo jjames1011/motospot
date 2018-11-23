078935ef1829a54d2a5fa83348d8eeebee89497c
Jonathan James <jjamesnwa1011@gmail.com>
2018-11-23 10:47:24 -0800

    got rid of the jquery cdn as its not being used and deleted geolocation usage

--- public/javascripts/basicscripts.js
+++ public/javascripts/basicscripts.js
@@ -6,20 +6,3 @@ function hideShow(ev) {
     x.style.display = 'block';
   }
 }
-
-function determinUsrLocation() {
-  let x = document.getElementById("demo");
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browswer");
-    }
-  }
-  function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
-  }
-  getLocation();
-}
-
-determinUsrLocation();
