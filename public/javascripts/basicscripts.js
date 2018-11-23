bc48d9640b1c407b1cb802b0bdbf298a1255a609
Jonathan James <jjamesnwa1011@gmail.com>
2018-11-23 10:40:09 -0800

    added a function to determin user location. Still need to add conditionals

--- public/javascripts/basicscripts.js
+++ public/javascripts/basicscripts.js
@@ -1,8 +1,25 @@
-function hideshow(ev) {
-  var x = ev.target.nextSibling;
+function hideShow(ev) {
+  let x = ev.target.nextSibling;
   if(x.style.display == 'block') {
     x.style.display = 'none';
   } else {
     x.style.display = 'block';
   }
 }
+
+function determinUsrLocation() {
+  let x = document.getElementById("demo");
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browswer");
+    }
+  }
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
+  }
+  getLocation();
+}
+
+determinUsrLocation();
